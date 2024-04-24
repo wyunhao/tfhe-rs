667560c4964b161e61fdc8a0c65def0e5aabbb68
Agnes Leroy <agnes.leroy@zama.ai>
2024-04-24 17:38:28 +0200

    feat(gpu): make PBS and ks execution parallel over available GPUs
    Only GPUs with peer access to GPU 0 can be used for this at the moment.
    Peer to peer copy is used if different GPUs are passed to memcpy_gpu_to_gpu
    A gpu offset is passed as new parameter to pbs and keyswitch to adjust the input/output index user per gpu.
    bsk and ksk are copied to all GPUs.
    The CI now tests & runs benchmarks on p3.8xlarge aws instances

--- backends/tfhe-cuda-backend/cuda/include/helper.h
+++ backends/tfhe-cuda-backend/cuda/include/helper.h
@@ -1,10 +1,16 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+extern bool p2p_enabled;
+
 extern "C" {
 int cuda_setup_multi_gpu();
 }
 
-void multi_gpu_checks(uint32_t gpu_count);
+int get_active_gpu_count(int num_inputs, int gpu_count);
+
+int get_num_inputs_on_gpu(int total_num_inputs, int gpu_index, int gpu_count);
+
+int get_gpu_offset(int total_num_inputs, int gpu_index, int gpu_count);
 
 #endif

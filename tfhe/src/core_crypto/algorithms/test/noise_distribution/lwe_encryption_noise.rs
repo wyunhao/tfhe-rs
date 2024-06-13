9c6eef8932b14d7f830a831f80ebe46ccce180b4
Arthur Meyre <arthur.meyre@zama.ai>
2024-06-13 15:43:43 +0000

    test multibit

--- tfhe/src/core_crypto/algorithms/test/noise_distribution/lwe_encryption_noise.rs
+++ tfhe/src/core_crypto/algorithms/test/noise_distribution/lwe_encryption_noise.rs
@@ -3,6 +3,7 @@ use crate::core_crypto::algorithms::misc::check_clear_content_respects_mod;
 use crate::core_crypto::commons::test_tools::{
     modular_distance, modular_distance_custom_mod, torus_modular_diff, variance,
 };
+use rayon::prelude::*;
 
 // This is 1 / 16 which is exactly representable in an f64 (even an f32)
 // 1 / 32 is too strict and fails the tests
@@ -271,3 +272,267 @@ create_parametrized_test!(random_noise_roundtrip {
     TEST_PARAMS_3_BITS_SOLINAS_U64,
     TEST_PARAMS_3_BITS_63_U64
 });
+
+use crate::shortint::parameters::multi_bit::p_fail_2_minus_64::ks_pbs_gpu::*;
+
+#[test]
+fn lwe_encrypt_multi_bit_pbs_decrypt_noise_distribution_custom_mod() {
+    use concrete_cpu_noise_model::gaussian_noise::noise::blind_rotate::variance_blind_rotate;
+    use concrete_cpu_noise_model::gaussian_noise::noise::multi_bit_blind_rotate::variance_multi_bit_blind_rotate;
+    let param_array = [
+        (
+            "PARAM_GPU_MULTI_BIT_GROUP_2_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64",
+            PARAM_GPU_MULTI_BIT_GROUP_2_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_GPU_MULTI_BIT_GROUP_2_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64",
+            PARAM_GPU_MULTI_BIT_GROUP_2_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_GPU_MULTI_BIT_GROUP_2_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64",
+            PARAM_GPU_MULTI_BIT_GROUP_2_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64",
+            PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64",
+            PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64",
+            PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_MULTI_BIT_GROUP_4_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64",
+            PARAM_MULTI_BIT_GROUP_4_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_MULTI_BIT_GROUP_4_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64",
+            PARAM_MULTI_BIT_GROUP_4_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64,
+        ),
+        (
+            "PARAM_MULTI_BIT_GROUP_4_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64",
+            PARAM_MULTI_BIT_GROUP_4_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64,
+        ),
+    ];
+
+    for (name, params) in param_array {
+        println!("{name}");
+        let mut rsc = TestResources::new();
+
+        let lwe_dimension = params.lwe_dimension;
+        let glwe_dimension = params.glwe_dimension;
+        let polynomial_size = params.polynomial_size;
+        let ciphertext_modulus = params.ciphertext_modulus;
+        let decomp_base_log = params.pbs_base_log;
+        let decomp_level_count = params.pbs_level;
+        let grouping_factor = params.grouping_factor;
+        let glwe_noise_distribution = params.glwe_noise_distribution;
+        let encoding_with_padding = get_encoding_with_padding(ciphertext_modulus);
+        let msg_modulus = (params.carry_modulus.0 * params.message_modulus.0) as u64;
+        let mut msg = msg_modulus;
+        let delta = encoding_with_padding / msg_modulus;
+
+        let num_samples = NB_TESTS * msg as usize;
+        let mut noise_samples = Vec::with_capacity(num_samples);
+
+        let small_lwe_sk = allocate_and_generate_new_binary_lwe_secret_key(
+            lwe_dimension,
+            &mut rsc.secret_random_generator,
+        );
+
+        let glwe_sk = allocate_and_generate_new_binary_glwe_secret_key(
+            glwe_dimension,
+            polynomial_size,
+            &mut rsc.secret_random_generator,
+        );
+
+        let big_lwe_sk = glwe_sk.as_lwe_secret_key();
+
+        let bsk = par_allocate_and_generate_new_lwe_multi_bit_bootstrap_key(
+            &small_lwe_sk,
+            &glwe_sk,
+            decomp_base_log,
+            decomp_level_count,
+            grouping_factor,
+            glwe_noise_distribution,
+            ciphertext_modulus,
+            &mut rsc.encryption_random_generator,
+        );
+
+        let mut fbsk = FourierLweMultiBitBootstrapKey::new(
+            bsk.input_lwe_dimension(),
+            bsk.glwe_size(),
+            bsk.polynomial_size(),
+            bsk.decomposition_base_log(),
+            bsk.decomposition_level_count(),
+            bsk.grouping_factor(),
+        );
+
+        par_convert_standard_lwe_multi_bit_bootstrap_key_to_fourier(&bsk, &mut fbsk);
+
+        let fft_once_at_the_end = false;
+
+        let expected_variance = Variance(variance_multi_bit_blind_rotate(
+            fbsk.input_lwe_dimension().0 as u64,
+            fbsk.glwe_size().to_glwe_dimension().0 as u64,
+            fbsk.polynomial_size().0 as u64,
+            decomp_base_log.0 as u64,
+            decomp_level_count.0 as u64,
+            64,
+            53,
+            glwe_noise_distribution.gaussian_variance().0,
+            grouping_factor.0 as u32,
+            fft_once_at_the_end,
+        ));
+
+        // let bsk = par_allocate_and_generate_new_lwe_bootstrap_key(
+        //     &small_lwe_sk,
+        //     &glwe_sk,
+        //     decomp_base_log,
+        //     decomp_level_count,
+        //     glwe_noise_distribution,
+        //     ciphertext_modulus,
+        //     &mut rsc.encryption_random_generator,
+        // );
+
+        // let mut fbsk = FourierLweBootstrapKey::new(
+        //     bsk.input_lwe_dimension(),
+        //     bsk.glwe_size(),
+        //     bsk.polynomial_size(),
+        //     bsk.decomposition_base_log(),
+        //     bsk.decomposition_level_count(),
+        // );
+
+        // par_convert_standard_lwe_bootstrap_key_to_fourier(&bsk, &mut fbsk);
+
+        // let expected_variance = Variance(variance_blind_rotate(
+        //     fbsk.input_lwe_dimension().0 as u64,
+        //     fbsk.glwe_size().to_glwe_dimension().0 as u64,
+        //     fbsk.polynomial_size().0 as u64,
+        //     decomp_base_log.0 as u64,
+        //     decomp_level_count.0 as u64,
+        //     64,
+        //     53,
+        //     glwe_noise_distribution.gaussian_variance().0,
+        // ));
+
+        let accumulator = generate_programmable_bootstrap_glwe_lut(
+            polynomial_size,
+            glwe_dimension.to_glwe_size(),
+            msg_modulus as usize,
+            ciphertext_modulus,
+            delta,
+            |x| x,
+        );
+
+        while msg != 0 {
+            msg -= 1;
+            println!("msg: {msg}");
+            let curr_samples = (0..NB_TESTS)
+                .into_par_iter()
+                .map(|idx| {
+                    let mut rsc = TestResources::new();
+
+                    let mut ct =
+                        LweCiphertext::new(0u64, lwe_dimension.to_lwe_size(), ciphertext_modulus);
+
+                    let plaintext = Plaintext(msg * delta);
+
+                    encrypt_lwe_ciphertext(
+                        &small_lwe_sk,
+                        &mut ct,
+                        plaintext,
+                        DynamicDistribution::new_gaussian_from_std_dev(StandardDev(0.0)),
+                        &mut rsc.encryption_random_generator,
+                    );
+
+                    assert!(check_encrypted_content_respects_mod(
+                        &ct,
+                        ciphertext_modulus
+                    ));
+
+                    let mut output_ct = LweCiphertext::new(
+                        0u64,
+                        fbsk.output_lwe_dimension().to_lwe_size(),
+                        ciphertext_modulus,
+                    );
+
+                    if fft_once_at_the_end {
+                        std_multi_bit_programmable_bootstrap_lwe_ciphertext(
+                            &ct,
+                            &mut output_ct,
+                            &accumulator,
+                            &bsk,
+                            ThreadCount(1 << grouping_factor.0),
+                            false,
+                        );
+                    } else {
+                        multi_bit_programmable_bootstrap_lwe_ciphertext(
+                            &ct,
+                            &mut output_ct,
+                            &accumulator,
+                            &fbsk,
+                            ThreadCount(1 << grouping_factor.0),
+                            false,
+                        );
+                    }
+                    // programmable_bootstrap_lwe_ciphertext(
+                    //     &ct,
+                    //     &mut output_ct,
+                    //     &accumulator,
+                    //     &fbsk,
+                    // );
+
+                    let decrypted = decrypt_lwe_ciphertext(&big_lwe_sk, &output_ct);
+
+                    let decoded = round_decode(decrypted.0, delta) % msg_modulus;
+
+                    // println!("#{idx} done");
+                    assert_eq!(msg, decoded);
+
+                    let torus_diff =
+                        torus_modular_diff(plaintext.0, decrypted.0, ciphertext_modulus);
+                    torus_diff
+                })
+                .collect::<Vec<_>>();
+
+            noise_samples.extend(curr_samples.into_iter());
+        }
+
+        let measured_variance = variance(&noise_samples);
+        let var_abs_diff = (expected_variance.0 - measured_variance.0).abs();
+        let tolerance_threshold = RELATIVE_TOLERANCE * expected_variance.0;
+        if measured_variance.0 > expected_variance.0 {
+            // assert!(
+            //     var_abs_diff < tolerance_threshold,
+            //     "Absolute difference for variance: {var_abs_diff}, \
+            //     tolerance threshold: {tolerance_threshold}, \
+            //     got variance: {measured_variance:?}, \
+            //     expected variance: {expected_variance:?}"
+            // );
+            if var_abs_diff > tolerance_threshold {
+                println!(
+                    "Absolute difference for variance: {var_abs_diff}, \
+                    tolerance threshold: {tolerance_threshold}, \n\
+                    got variance: {measured_variance:?}, \n\
+                    expected variance: {expected_variance:?}"
+                );
+
+                println!("FAIL ===========================================================")
+            } else {
+                println!("Measured variance is in the expected range : OK");
+                println!("Measured: {measured_variance:?}");
+                println!("Expected: {expected_variance:?}");
+            }
+        } else {
+            println!("Measured variance is smaller than what would be expected : OK");
+            println!("Measured: {measured_variance:?}");
+            println!("Expected: {expected_variance:?}");
+        }
+
+        println!("{name} OK");
+    }
+}
--- tfhe/src/shortint/parameters/multi_bit/p_fail_2_minus_64/ks_pbs_gpu.rs
+++ tfhe/src/shortint/parameters/multi_bit/p_fail_2_minus_64/ks_pbs_gpu.rs
@@ -159,3 +159,79 @@ pub const PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64:
     grouping_factor: LweBskGroupingFactor(3),
     deterministic_execution: false,
 };
+
+// p-fail = 2^-65.353, algorithmic cost ~ 98, 2-norm = 3
+pub const PARAM_MULTI_BIT_GROUP_4_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64: MultiBitPBSParameters =
+    MultiBitPBSParameters {
+        lwe_dimension: LweDimension(788),
+        glwe_dimension: GlweDimension(4),
+        polynomial_size: PolynomialSize(512),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            7.85955004091113e-06,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.845267479601915e-15,
+        )),
+        pbs_base_log: DecompositionBaseLog(22),
+        pbs_level: DecompositionLevelCount(1),
+        ks_base_log: DecompositionBaseLog(4),
+        ks_level: DecompositionLevelCount(3),
+        message_modulus: MessageModulus(2),
+        carry_modulus: CarryModulus(2),
+        max_noise_level: MaxNoiseLevel::new(3),
+        log2_p_fail: -65.353,
+        ciphertext_modulus: CiphertextModulus::new_native(),
+        encryption_key_choice: EncryptionKeyChoice::Big,
+        grouping_factor: LweBskGroupingFactor(4),
+        deterministic_execution: false,
+    };
+// p-fail = 2^-65.196, algorithmic cost ~ 92, 2-norm = 5
+pub const PARAM_MULTI_BIT_GROUP_4_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64: MultiBitPBSParameters =
+    MultiBitPBSParameters {
+        lwe_dimension: LweDimension(844),
+        glwe_dimension: GlweDimension(1),
+        polynomial_size: PolynomialSize(2048),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.9907869683339986e-06,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.845267479601915e-15,
+        )),
+        pbs_base_log: DecompositionBaseLog(21),
+        pbs_level: DecompositionLevelCount(1),
+        ks_base_log: DecompositionBaseLog(3),
+        ks_level: DecompositionLevelCount(5),
+        message_modulus: MessageModulus(4),
+        carry_modulus: CarryModulus(4),
+        max_noise_level: MaxNoiseLevel::new(5),
+        log2_p_fail: -65.196,
+        ciphertext_modulus: CiphertextModulus::new_native(),
+        encryption_key_choice: EncryptionKeyChoice::Big,
+        grouping_factor: LweBskGroupingFactor(4),
+        deterministic_execution: false,
+    };
+// p-fail = 2^-64.167, algorithmic cost ~ 766, 2-norm = 9
+pub const PARAM_MULTI_BIT_GROUP_4_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64: MultiBitPBSParameters =
+    MultiBitPBSParameters {
+        lwe_dimension: LweDimension(984),
+        glwe_dimension: GlweDimension(1),
+        polynomial_size: PolynomialSize(8192),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.671498718807819e-07,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.168404344971009e-19,
+        )),
+        pbs_base_log: DecompositionBaseLog(14),
+        pbs_level: DecompositionLevelCount(2),
+        ks_base_log: DecompositionBaseLog(3),
+        ks_level: DecompositionLevelCount(6),
+        message_modulus: MessageModulus(8),
+        carry_modulus: CarryModulus(8),
+        max_noise_level: MaxNoiseLevel::new(9),
+        log2_p_fail: -64.167,
+        ciphertext_modulus: CiphertextModulus::new_native(),
+        encryption_key_choice: EncryptionKeyChoice::Big,
+        grouping_factor: LweBskGroupingFactor(4),
+        deterministic_execution: false,
+    };
